3423f9b16550127e0b1663d0c761e58934675bd3
Balazs Kegl <balazs.kegl@gmail.com>
2017-06-13 17:18:17 +0200

    cosmetics

--- prepare_data.py
+++ prepare_data.py
@@ -34,6 +34,8 @@ df_public_train, df_public_test = train_test_split(
 df_public_train.to_csv(os.path.join('data', 'public_train.csv'), index=False)
 df_public_test.to_csv(os.path.join('data', 'public_test.csv'), index=False)
 
+
+# copy starting kit files to <ramp_kits_dir>/<ramp_name>/data
 copyfile(
     os.path.join('data', 'public_train.csv'),
     os.path.join(ramp_kits_dir, ramp_name, 'data', 'train.csv')
@@ -42,3 +44,5 @@ copyfile(
     os.path.join('data', 'public_test.csv'),
     os.path.join(ramp_kits_dir, ramp_name, 'data', 'test.csv')
 )
+
+
